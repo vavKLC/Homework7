03b10f8c6ef13d0e74bb9260a632b8488b9fa4d3
vavKLC <94033595+vavKLC@users.noreply.github.com>
2021-11-29 05:41:58 -0600

    Update Medic.java

--- src/com/company/Medic.java
+++ src/com/company/Medic.java
@@ -3,6 +3,6 @@ package com.company;
 public class Medic extends Hero{
     @Override
     public String applySuperAbility(String superAbilityType) {
-        return "Medic применил суперспособность Global Heal ";// + superAbilityType;
+        return "Medic применил суперспособность Global Heal ";
     }
 }
