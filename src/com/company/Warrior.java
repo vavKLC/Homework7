8d181a16e467224653b0abb23665339b6396f68e
vavKLC <94033595+vavKLC@users.noreply.github.com>
2021-11-29 05:42:20 -0600

    Update Warrior.java

--- src/com/company/Warrior.java
+++ src/com/company/Warrior.java
@@ -3,6 +3,6 @@ package com.company;
 public class Warrior extends Hero{
     @Override
     public String applySuperAbility(String superAbilityType) {
-        return "Warrior применил способность Battle Rage ";// + superAbilityType;
+        return "Warrior применил способность Battle Rage ";
     }
 }
